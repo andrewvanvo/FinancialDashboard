1b136bb20e1a088d9332eedfd5f5a555cf611591
andrewvanvo <andrew.van.vo@gmail.com>
2022-02-13 15:15:46 -0500

    Base HBS dir struc laid out and established in app.js

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 'use strict'
 
+//Setting variables
 const express = require("express");
 const axios = require('axios');
+const exprHBS = require('express-handlebars')
 const app = express();
 const PORT = 3000 //process.env.PORT || 80;
 
+//HBS templating view engine
+app.engine('handlebars', exprHBS());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+//HBS routing
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+//JSON parsing for http requests
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
 
+//Base static
 app.use('/', express.static('public'));
 
 
