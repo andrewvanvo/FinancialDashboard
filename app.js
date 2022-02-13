7c114731a2dd238a17c9e3cf4323885f45e6e0be
andrewvanvo <andrew.van.vo@gmail.com>
2022-02-13 02:04:41 -0500

    Changes package json

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 80;
 
 app.use(express.json());
 app.use(express.urlencoded({
