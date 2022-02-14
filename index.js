1ac3a4ea5afb58a7cfa979c983c9f99685ad1955
andrewvanvo <andrew.van.vo@gmail.com>
2022-02-13 21:33:15 -0500

    renaming tryin to resolve routing issue

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
+'use strict'
+
 const express = require('express')
 const { engine } = require ('express-handlebars');
+const path = require('path')
 
 const app = express();
 
 const PORT = process.env.PORT || 3000
 
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set("views", "./views");
 
+
+
 app.get('/', (req, res) => {
     res.render('home');
 });
+
+
 
 
 app.listen(PORT, () => {
